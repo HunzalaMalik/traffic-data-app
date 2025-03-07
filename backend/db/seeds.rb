1f782008bd7f77708a9bd6e334a19039042ca507
Hunzala Malik <hunzalamalik@gmail.com>
2025-03-08 02:11:22 +0500

    Add Dockerfile and seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,36 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+VehicleTypeTraffic.destroy_all
+CountryTraffic.destroy_all
+
+countries = [
+  { country: "United States", count: 500 },
+  { country: "Canada", count: 300 },
+  { country: "United Kingdom", count: 400 },
+  { country: "Germany", count: 350 },
+  { country: "France", count: 375 },
+  { country: "India", count: 700 },
+  { country: "Pakistan", count: 600 },
+  { country: "Australia", count: 275 },
+  { country: "Japan", count: 450 },
+  { country: "Brazil", count: 500 }
+]
+
+countries.each do |country_data|
+  country = CountryTraffic.create!(country_data)
+
+  if country.country == "United States"
+    country.vehicle_type_traffics.create!([
+      { vehicle_type: "Car", count: 200 },
+      { vehicle_type: "Bike", count: 100 }
+    ])
+  elsif country.country == "United Kingdom"
+    country.vehicle_type_traffics.create!([
+      { vehicle_type: "Bus", count: 150 },
+      { vehicle_type: "Car", count: 100 }
+    ])
+  elsif country.country == "India"
+    country.vehicle_type_traffics.create!([
+      { vehicle_type: "Bike", count: 300 },
+      { vehicle_type: "Bus", count: 200 }
+    ])
+  end
+end
